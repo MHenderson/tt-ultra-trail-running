49b528b7076ca252fbf32977eec055a47bfe43c2
Matthew Henderson <matthew.james.henderson@gmail.com>
2025-02-20 08:32:01 +0000

    Add top_nationalities target.

--- _targets.R
+++ _targets.R
@@ -1,12 +1,22 @@
 library(targets)
 
 tar_option_set(
-  packages = c("readr", "tibble")
+  packages = c("dplyr", "readr", "tibble")
 )
 
 list(
   tar_target(
        name = ultra_rankings,
     command = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/ultra_rankings.csv')
+  ),
+  tar_target(
+       name = top_nationalities,
+    command = {
+      top_nationalities <- ultra_rankings |>
+	count(nationality) |>
+	arrange(desc(n)) |>
+	head(16) |>
+	pull(nationality)
+    }
   )
 )
